7cb57b5aa4b73cacf8237394757a83f10d319061
Шарунов Владислав <newrussian73@mail.ru>
2021-05-24 21:32:19 +0400

    Разделение открытие/закрытие браузера.

--- src/test/java/Tests/GoogleTest.java
+++ src/test/java/Tests/GoogleTest.java
@@ -1,20 +1,40 @@
 package Tests;
 
 
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class GoogleTest {
 
+    private static WebDriver driver;
+
+
+    @BeforeAll
+    public static void init() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("start-maximized");
+        driver = new ChromeDriver(options);
+
+
+    }
+
     @Test
     public void test1() {
-        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
-        WebDriver driver = new ChromeDriver();
         driver.get("http://google.com");
         driver.findElement(By.cssSelector("input.gLFyf.gsfi")).sendKeys("Калькулятор", Keys.ENTER);
+
+    }
+
+    @AfterAll
+    public static void teardown() {
         driver.quit();
     }
 }
