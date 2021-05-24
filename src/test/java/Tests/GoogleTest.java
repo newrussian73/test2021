b9320e3ee2644e01e6f754b7a0855e48add9deb5
Шарунов Владислав <newrussian73@mail.ru>
2021-05-24 21:01:11 +0400

    Добавлена рыба первого проекта

---
+++ src/test/java/Tests/GoogleTest.java
@@ -0,0 +1,17 @@
+package Tests;
+
+
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class GoogleTest {
+
+    @Test
+    public void test1() {
+        System.setProperty("webdriver.chrome.driver","src/test/resources/chromedriver.exe");
+        WebDriver driver = new ChromeDriver();
+        driver.get("http://google.com");
+        driver.quit();
+    }
+}
